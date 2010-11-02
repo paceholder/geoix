ff515d7824d0200cd40846ab9f47cab68feb11d5
paceholder <paceholder@paceholder-desktop.(none)>
2010-11-02 10:19:06 +0500

    Datamanager

---
+++ datamanager/datamanager.cpp
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------
+//    This file is part of Geoix.
+//
+//    Copyright (C) 2010 Dmitriy Pinaev
+//
+//    Geoix is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Geoix is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Geoix.  If not, see <http://www.gnu.org/licenses/>.
+//
+//    e-mail: prof-x@inbox.ru
+//------------------------------------------------------------------------
+
+
+
+
+
+#include "datamanager.h"
+
+
+gxDataManager::gxDataManager()
+{
+}
---
+++ datamanager/datamanager.pro
@@ -0,0 +1,17 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2010-10-31T16:20:06
+#
+#-------------------------------------------------
+
+QT       -= gui
+
+TARGET = datamanager
+TEMPLATE = lib
+
+DEFINES += DATAMANAGER_LIBRARY
+
+SOURCES += datamanager.cpp
+
+HEADERS += datamanager.h\
+        datamanager_global.h
